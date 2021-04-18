b63dc3fd59377baeab719dd844a7de293b5dba2f
Fabian Castro <facaslo.99@gmail.com>
2021-04-18 10:55:22 -0500

    Se cambió la estructura del directorio y se añadieron las librerias localmente

---
+++ src/my.packages/loginForm.py
@@ -0,0 +1,31 @@
+import sys,os,pathlib
+from pathlib import Path
+flaskwtf_path = base_path = Path(__file__).parent.parent.parent
+flask_path = os.path.join(base_path, '.\\lib\\wtforms' )
+sys.path.append(flaskwtf_path)
+
+from wtforms import Form
+from wtforms import StringField, TextField, PasswordField, HiddenField
+from wtforms.fields.html5 import EmailField
+from wtforms import validators
+
+def length_honeypot(form, field):
+    if len(field.data) > 0:
+        raise validators.ValidationError('El campo debe estar vacío')
+
+class LoginForm(Form):
+    username = StringField('Username', 
+            [
+                validators.DataRequired(message= 'El username es requerido'),
+                validators.length(min=4, max=25, message="¡Ingrese un username valido!")                 
+            ]
+            )
+   
+    password = PasswordField('Password',
+            [
+                validators.DataRequired(message= 'La password es requerida')             
+            ]
+            )
+
+    """ Campo para hacer caer a bots """
+    honeypot = HiddenField('', [length_honeypot])
